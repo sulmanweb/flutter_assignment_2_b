b0a5938239914118a62d05fbbf9522eaf924a59e
Sulman Baig <sulman@hey.com>
2023-10-08 20:45:57 +0500

    Adding login screen and init repo

---
+++ android/app/src/main/kotlin/com/example/assignment_2_b/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.assignment_2_b
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screens/login.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text(
+          'Assignment 2b Login',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.amberAccent,
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              // Email
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Enter your email',
+                  labelStyle: TextStyle(color: Colors.black),
+                  hintStyle: TextStyle(color: Colors.black),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(100)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(100)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
+                  ),
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+              // --Email
+              const SizedBox(height: 16.0),
+              // Password
+              TextFormField(
+                obscureText: true,
+                obscuringCharacter: '%',
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  labelStyle: TextStyle(color: Colors.black),
+                  hintStyle: TextStyle(color: Colors.black),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(100)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(100)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
+                  ),
+                  prefixIcon: Icon(Icons.lock),
+                ),
+              ),
+              // --Password
+              const SizedBox(height: 16.0),
+              // Login Button
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    textStyle: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100),
+                      side: const BorderSide(width: 2, color: Colors.black),
+                    ),
+                    minimumSize: Size(double.infinity, 60)),
+                child: const Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
